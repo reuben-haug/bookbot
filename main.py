b11e386fadcc942665ebf9322c5a91b631085e8a
reuben-haug <reuben.haug@gmail.com>
2023-10-15 19:32:00 -0700

    Added use of collections.Counter for efficient counting.  Docstring additions.

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
+"""Reads the contents of a text file, printing a report that returns the number of times each word occurs in the text, as well as a count of each time a letter appeared in the text"""
+#Required imports
+import collections
+
 #read the contents of frankenstein.txt
 with open("./books/frankenstein.txt") as f:
 
     #contents of the file as a string
     file_contents = f.read()
-    
-    #return the number of words
+
     def count_words(file_contents):
+        """return the number of words"""
+
         words = file_contents.split()
         return len(words)
-
-    #return the number of characters, ignoring case
+    
     def count_letters(file_contents):
-        #initialize an empty return dictionary
-        letter_dict = {}
-        for char in file_contents:
-            #convert to lowercase
-            lower_char = char.lower()
-            #only alphanumeric characters
-            if char.isalpha() == True:
-                if lower_char in letter_dict:
-                    letter_dict[lower_char] += 1
-                else:
-                    letter_dict[lower_char] = 1
-        return letter_dict
+        """return the number of alphabetical characters, ignoring case"""
+
+        #only count lowercase alphabetical characters
+        processed_file_contents = [char.lower() for char in file_contents if char.isalpha()]
+
+        #create a Counter object to tally
+        counter = collections.Counter(processed_file_contents)
+
+        #flattern into a regular dictionary
+        return dict(counter)
     
     def text_report():
+        """Calls word_count & char_dict functions, printing the results"""
+
         #get the word count dictionary
         word_count = count_words(file_contents)
+
         #get the character count dictionary
         char_dict = count_letters(file_contents)
 
@@ -36,6 +41,7 @@ with open("./books/frankenstein.txt") as f:
 
         #sort the dictionary by highest character count
         sorted_char_list = sorted(char_dict.items(), key=lambda x:x[1], reverse=True)
+
         #step through the character dictionary & print the formatted results for each character
         for entry in sorted_char_list:
             (char, count) = entry
@@ -43,4 +49,5 @@ with open("./books/frankenstein.txt") as f:
 
         #footer format for the book content report
         print(f'\n--- End report ---')
+        
 text_report()
