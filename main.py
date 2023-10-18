c61ca862dfd6a8632c1971f8da77c1ea0d419fb3
reuben-haug <reuben.haug@gmail.com>
2023-10-17 21:29:37 -0700

    -Separated reading the text file into its own function -Pass the 'file_path' as an argument to the functions that need it -Removed the global variable 'file_contents' -Added main block to call the function

--- main.py
+++ main.py
@@ -1,53 +1,47 @@
 """Reads the contents of a text file, printing a report that returns the number of times each word occurs in the text, as well as a count of each time a letter appeared in the text"""
-#Required imports
+
 import collections
 
-#read the contents of frankenstein.txt
-with open("./books/frankenstein.txt") as f:
+def read_file(file_path):
+    """Read the contents of the file at the given path."""
 
-    #contents of the file as a string
-    file_contents = f.read()
+    with open(file_path) as f:
+        return f.read()
 
-    def count_words(file_contents):
-        """return the number of words"""
+def count_words(file_contents):
+    """Return the number of words in the text."""
 
-        words = file_contents.split()
-        return len(words)
+    words = file_contents.split()
+    return len(words)
     
-    def count_letters(file_contents):
-        """return the number of alphabetical characters, ignoring case"""
+def count_letters(file_contents):
+    """Tally & return the number of alphabetical characters, ignoring case."""
 
-        #only count lowercase alphabetical characters
-        processed_file_contents = [char.lower() for char in file_contents if char.isalpha()]
+    processed_file_contents = [char.lower() for char in file_contents if char.isalpha()]
+    counter = collections.Counter(processed_file_contents)
+    return dict(counter)
 
-        #create a Counter object to tally
-        counter = collections.Counter(processed_file_contents)
+def generate_text_report(file_path):
+    """Generate a report of word and character counts from a file."""
 
-        #flattern into a regular dictionary
-        return dict(counter)
+    file_contents = read_file(file_path)
+    word_count = count_words(file_contents)
+    char_dict = count_letters(file_contents)
+
+    # Sort the dictionary by highest character count
+    sorted_char_list = sorted(char_dict.items(), key=lambda x:x[1], reverse=True)
+
+    # Header format for the report
+    print(f'--- Begin report of {file_path} ---\n')
+    print(f'{word_count} words found in the document.\n')
+
+    # Step through the character dictionary, print the results
+    for char, count in sorted_char_list:
+        print(f'The "{char}" character was found {count} times.')
     
-    def text_report():
-        """Calls word_count & char_dict functions, printing the results"""
-
-        #get the word count dictionary
-        word_count = count_words(file_contents)
-
-        #get the character count dictionary
-        char_dict = count_letters(file_contents)
-
-        #header format for the book content report
-        print(f'--- Begin report of {f.name} ---\n')
-        print(f'{word_count} words found in the document\n')
-
-        #sort the dictionary by highest character count
-        sorted_char_list = sorted(char_dict.items(), key=lambda x:x[1], reverse=True)
-
-        #step through the character dictionary & print the formatted results for each character
-        for entry in sorted_char_list:
-            (char, count) = entry
-            print(f'The "{char}" character was found {count} times')
-
-        #footer format for the book content report
-        print(f'\n--- End report ---')
+    # Footer format for the reports
+    print(f'\n--- End report ---')
         
-text_report()
+if __name__ == "__main__":
+    file_path = "./books/frankenstein.txt"
+    generate_text_report(file_path)
